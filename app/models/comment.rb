88b7fd719b1032439228a0256a39c87b5416165e
Tony Lattke <tonylattke@gmail.com>
2014-07-31 11:06:17 +0200

    validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+	belongs_to :post
+
+	validates :name, :text, :presence => true
+	validates :name, :length => { :minimum => 2 }
+	validates :text, :length => { :minimum => 2 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 
+	validates :name, :text, :presence => true
+	validates :name, :length => { :minimum => 2 }
+	validates :text, :length => { :minimum => 2 }
+
 	default_scope { order('created_at DESC') }	
 end
